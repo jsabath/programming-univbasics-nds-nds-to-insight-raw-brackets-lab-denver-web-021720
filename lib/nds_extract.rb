5a605c9292ee02ea93f83f4ea98ae4846f058752
Justin Sabath <justin.sabath@aol.com>
2020-01-30 19:52:24 +0000

    Automatically backed up by Learn

--- lib/nds_extract.rb
+++ lib/nds_extract.rb
@@ -2,6 +2,25 @@ $LOAD_PATH.unshift(File.dirname(__FILE__))
 require 'directors_database'
 
 def directors_totals(nds)
+    director_index = 0
+    totals = {}
+
+    while director_index < nds.length do
+        director_name = nds[director_index][:name]
+        totals[director_name] = 0
+        movie_index = 0
+
+        while movie_index < nds[director_index][:movies].length do
+            totals[director_name] += nds[director][:movies][movie_index][:worldwide_gross]
+            movie_index += 1
+        end
+        director_index += 1
+    end
+    totals
+end
+
+'''
+def directors_totals(nds)
   director_index = 0
   totals = {}
 
@@ -18,3 +37,4 @@ def directors_totals(nds)
   end
   totals
 end
+'''
